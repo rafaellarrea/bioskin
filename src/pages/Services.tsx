1958aede6c40f4f9de27257b214213b0e88898bb
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-19 12:10:42 -0500

    correccion promo mes

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -81,9 +81,8 @@ const Services = () => {
 
  {/*PROMOCION DEL MES*/}       
 	<div className="mt-16 text-center animate-fade-in" data-aos="fade-up">
-  	{/*<p className="uppercase text-2xl font-semibold mb-4"> Promoción del Mes*/}
-	<h3 className="text-2xl font-semibold mb-4">Promoción del mes</h3>
-  	</p>
+  	<p className="uppercase text-2xl font-semibold mb-4"> Promoción del Mes
+	  	</p>
  	 <div className="card mx-auto max-w-md p-6 border-2 border-[#deb887] mb-12">
     	<h4 className="text-xl font-semibold mb-2">Hollywood Peel + Hidratación Profunda</h4>
    	 <p className="text-gray-500 text-sm mb-4">Unifica tono, revitaliza y nutre tu piel 	profundamente en una sola sesión.</p>
